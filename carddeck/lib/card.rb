8e815aad7bf14e2e118db762810d2cef172abd91
David Mowrer <david.mowrer@comcast.net>
2014-08-18 11:04:59 -0600

    In class project/card deck

---
+++ carddeck/lib/card.rb
@@ -0,0 +1,21 @@
+# this comment removes a Rubocop offense
+class Card
+  attr_reader :suit, :value
+
+  def initialize(suit, value)
+    fail ArgumentError, 'Invalid suit' unless valid_suit?(suit)
+    fail ArgumentError, 'Invalid value' unless valid_value?(value)
+    @suit = suit
+    @value = value
+  end
+
+  private
+
+  def valid_suit?(suit)
+    [:heart, :diamond, :club, :spade].include?(suit)
+  end
+
+  def valid_value?(value)
+    ((2..10).to_a + %w(J Q K A)).include?(value)
+  end
+end
